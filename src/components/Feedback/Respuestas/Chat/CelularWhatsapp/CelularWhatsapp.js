afdb2952489aefc801f269c031327f3e302d8ab0
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-10-15 20:26:01 -0300

    mejoras celular

--- src/components/Feedback/Respuestas/Chat/CelularWhatsapp/CelularWhatsapp.js
+++ src/components/Feedback/Respuestas/Chat/CelularWhatsapp/CelularWhatsapp.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import classNames from 'classnames'
 import './CelularWhatsapp.css'
 import { useSelector } from 'react-redux'
+import { format, parseISO } from 'date-fns'
+import { Icon, InlineIcon } from '@iconify/react'
+import phone from '@iconify/icons-mdi/phone'
+import iconoVisto from '@iconify/icons-mdi/check-all'
+import iconoVolver from '@iconify/icons-mdi/arrow-back'
 
 export const CelularWhatsapp = ({ mensajes }) => {
 
   const { nombreUsuario } = useSelector(state => state.login)
+  const [hora, setHora] = useState(new Date())
+
+  useEffect(() => {
+    const actualizarHora = setInterval(() => setHora(new Date()), 1000)
+    return () => clearInterval(actualizarHora)
+  }, [])
 
   return (
     <div className="CelularWhatsapp">
       <div className="CelularWhatsapp__celular">
         <div className="CelularWhatsapp__pantalla">
           <div className="CelularWhatsapp__barra_estado">
-            19:11
+            {format(hora, 'HH:mm')}
           </div>
           <div className="CelularWhatsapp__barra_app">
-            Gaby de {nombreUsuario}
+            <div className="CelularWhatsapp__barra_app_izquierda">
+              <Icon icon={iconoVolver} className="CelularWhatsapp__icono_volver" />
+              <div className="CelularWhatsapp___barra_app_avatar" />
+              <div className="CelularWhatsapp__barra_app_contenedor_nombre">
+                <div className="CelularWhatsapp__barra_app_nombre">
+                  Gaby de {nombreUsuario.split(' ')[0]}
+                </div>
+                <div className="CelularWhatsapp__barra_app_estado">
+                  en línea
+                </div>
+              </div>
+            </div>
+            <div className="CelularWhatsapp__barra_app_iconos">
+              <Icon icon={phone} />
+            </div>
           </div>
           <div className="CelularWhatsapp__contenedor_mensajes">
             {mensajes.map(mensaje => (
@@ -29,9 +54,14 @@ export const CelularWhatsapp = ({ mensajes }) => {
               >
                 <div className="CelularWhatsapp__texto">
                   {mensaje.message}
+                  <div className="CelularWhatsapp__hora">
+                    {format(parseISO(mensaje.timestamp), 'HH:mm')}
+                    {mensaje.type !== 'bot' && <InlineIcon className="CelularWhatsapp__icono_visto" icon={iconoVisto} />}
+                  </div>
                 </div>
-                <div className="CelularWhatsapp__hora">
-                  {mensaje.timestamp}
+                <div className="CelularWhatsapp__hora_visible">
+                  {format(parseISO(mensaje.timestamp), 'HH:mm')}
+                  {mensaje.type !== 'bot' && <InlineIcon className="CelularWhatsapp__icono_visto" icon={iconoVisto} />}
                 </div>
               </div>
             ))}
