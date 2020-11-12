47489f93268ec6e50d50d3cf962f21b4b1549159
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-11-11 22:39:07 -0300

    agrega boton nueva busqueda

--- src/components/Feedback/Busqueda/ResultadosBusqueda/ResultadosBusqueda.js
+++ src/components/Feedback/Busqueda/ResultadosBusqueda/ResultadosBusqueda.js
@@ -1,22 +1,32 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { limpiaBusqueda } from '../../../../redux/ducks/busqueda'
 import ResultadoBusqueda from './ResultadoBusqueda'
 import './ResultadosBusqueda.css'
 
 const ResultadosBusqueda = () => {
 
   const { resultadosBusqueda } = useSelector(state => state.busqueda)
+  const dispatch = useDispatch()
 
   console.log(resultadosBusqueda)
 
   return (
     <div className="ResultadosBusqueda">
-      {resultadosBusqueda.map((r, i) => (
-        <ResultadoBusqueda
-          key={`resultado-busqueda-${i}`}
-          resultado={r}
-        />
-      ))}
+      <div className="ResultadosBusqueda__contenedor_resultados">
+        {resultadosBusqueda.map((r, i) => (
+          <ResultadoBusqueda
+            key={`resultado-busqueda-${i}`}
+            resultado={r}
+          />
+        ))}
+      </div>
+      <button
+        className="ResultadosBusqueda__boton_nueva_busqueda"
+        onClick={() => dispatch(limpiaBusqueda())}
+      >
+        Nueva búsqueda
+      </button>
     </div>
   )
 }
