7ca88da52efb9136b5af4a484ccc0cb289f366e5
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-03-01 13:51:16 -0300

    mejora usabilidad de input para busquedas sucesivas

--- README.md
+++ README.md
@@ -10,18 +10,13 @@ varios chats:
 - http://localhost:3000/chat/103/86707
 
 - REVISAR HORAS QUE parece que estan UTC en chat 2 hay que restarles 4
-- agregar disclaimer al pdf
-- en el pdf, ser explicito en las fechas de emision (este reporte consdiera de xx a xx, con horas en ambos casos) y ademas poner fecha de emision en el footer yo creo
+- mejorar carga de chat con skeletons
 - en las cajitas de busqueda no poner skeletons, solo mantener barra superior
-- crear pdf de vista de uso
-- cambiar boton de nueva busqueda por un input
-- agregar x en filtros
 - exportar tabla no lo entiende felipe, podría destacarse con borde la tabla
-- agregar error 500 en login: servicio no diponible en estos momentos, por favor intenta más tarde
 - link feedback classic
 - modo oscuros
-- mejorar carga de chat con skeletons
 - sankey
-- vista alertas (revisar api en https://github.com/mquezada/botlab-encuestas/blob/master/app/docs/api.md)
 
-
+Hechos:
+- agregar disclaimer al pdf
+- en el pdf, ser explicito en las fechas de emision (este reporte consdiera de xx a xx, con horas en ambos casos) y ademas poner fecha de emision en el footer yo creo
--- src/components/Feedback/Busqueda/ResultadosBusqueda/ResultadosBusqueda.js
+++ src/components/Feedback/Busqueda/ResultadosBusqueda/ResultadosBusqueda.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { comienzaBusqueda, guardaResultadosBusqueda } from '../../../../redux/ducks/busqueda'
 import TarjetaResultadoBusqueda from './TarjetaResultadoBusqueda'
@@ -16,6 +16,7 @@ const ResultadosBusqueda = () => {
   const [terminoNuevaBusqueda, setTerminoNuevaBusqueda] = useState(termino)
   const dispatch = useDispatch()
   const history = useHistory()
+  const inputBusquedaRef = useRef()
 
   useEffect(() => {
     dispatch(comienzaBusqueda())
@@ -25,9 +26,9 @@ const ResultadosBusqueda = () => {
       })
   }, [dispatch, termino])
 
-  if (!resultadosBusqueda) {
-    return <LoaderResultadosBusqueda />
-  }
+  useEffect(() => {
+    inputBusquedaRef.current.focus()
+  }, [resultadosBusqueda])
 
   const buscar = e => {
     e.preventDefault()
@@ -36,24 +37,34 @@ const ResultadosBusqueda = () => {
 
   return (
     <div className="ResultadosBusqueda">
-      <div className="ResultadosBusqueda__contenedor_resultados">
-        {resultadosBusqueda.map((r, i) => (
-          <TarjetaResultadoBusqueda
-            key={`resultado-busqueda-${i}`}
-            resultado={r}
-            posicion={i + 1}
-          />
-        ))}
-      </div>
+      {resultadosBusqueda
+        ? <div className="ResultadosBusqueda__contenedor_resultados">
+            {resultadosBusqueda.map((r, i) => (
+              <TarjetaResultadoBusqueda
+                key={`resultado-busqueda-${i}`}
+                resultado={r}
+                posicion={i + 1}
+              />
+            ))}
+          </div>
+        : <LoaderResultadosBusqueda />
+      }
       <div className="ResultadosBusqueda__superior">
         <div className="ResultadosBusqueda__encontrados">
-          Buscaste <span className="ResuladosBusqueda__termino">"{termino}"</span> - {resultadosBusqueda.length} chats encontrados
+          {resultadosBusqueda
+            ? <>
+                Buscaste <span className="ResuladosBusqueda__termino">"{termino}"</span> - {resultadosBusqueda.length} chats encontrados
+              </>
+            : <p>Buscando...</p>
+          }
         </div>
         <form onSubmit={buscar}>
           <input
             className="ResultadosBusqueda__input_nueva_busqueda"
             onChange={e => setTerminoNuevaBusqueda(e.target.value)}
             value={terminoNuevaBusqueda}
+            disabled={!resultadosBusqueda}
+            ref={inputBusquedaRef}
           />
           <button className="ResultadosBusqueda__boton_nueva_busqueda" type="submit">
             <Icon icon={iconoBuscar} />
