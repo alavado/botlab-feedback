0dfb21a4ccb2528e723e44ca17c5e9404db1d52c
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-03-03 12:49:58 -0300

    loader y animaciones para mini dashboard

--- README.md
+++ README.md
@@ -10,15 +10,15 @@ varios chats:
 - http://localhost:3000/chat/103/86707
 
 - REVISAR HORAS QUE parece que estan UTC en chat 2 hay que restarles 4
-- cambiar skeleton de mini dashboard por loader
 - mejorar carga de chat con skeletons
 - exportar tabla no lo entiende felipe, podría destacarse con borde la tabla
 - link feedback classic
-- modo oscuros
 - sankey
 
 Hechos:
 - agregar disclaimer al pdf
 - en el pdf, ser explicito en las fechas de emision (este reporte consdiera de xx a xx, con horas en ambos casos) y ademas poner fecha de emision en el footer yo creo
+- cambiar skeleton de mini dashboard por loader
+- modo oscuros
 - en las cajitas de busqueda no poner skeletons, solo mantener barra superior
 
--- src/components/ModalAyuda/ModalAyuda.js
+++ src/components/ModalAyuda/ModalAyuda.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import video from '../../assets/images/demo_filtros.gif'
+import videoDark from '../../assets/images/demo_filtros_dark.gif'
 import Icon from '@iconify/react'
 import iconoCerrar from '@iconify/icons-mdi/close'
 import './ModalAyuda.css'
+import { useSelector } from 'react-redux'
+import { ESQUEMA_OSCURO } from '../../redux/ducks/opciones'
 
 const ModalAyuda = ({ cerrar }) => {
+
+  const { esquema } = useSelector(state => state.opciones)
+
   return (
     <div className="ModalAyuda">
       <button className="ModalAyuda__boton_cerrar" title="Cerrar ayuda" onClick={cerrar}>
         <Icon icon={iconoCerrar} />
       </button>
-      <img className="ModalAyuda__gif" src={video} alt="Explicación filtros" />
+      <img
+        className="ModalAyuda__gif"
+        src={esquema === ESQUEMA_OSCURO ? videoDark : video}
+        alt="Explicación uso de filtros"
+      />
       <div className="ModalAyuda__bajada">
         <h2 className="ModalAyuda__titulo">Cómo usar los filtros</h2>
         <ul className="ModalAyuda__explicación">
