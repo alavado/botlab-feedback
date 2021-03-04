1f23b554255534396d345aadbefa24ec7590693f
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-03-04 11:15:23 -0300

    dev en localhost o netlify

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-export const API_ROOT = process.env.NODE_ENV === 'development'
+export const API_ROOT = window.location.href.indexOf('dev') > 0 || window.location.href.indexOf('localhost') > 0
   ? 'https://api.dev.botlab.cl'
   : 'https://api.botlab.cl'
