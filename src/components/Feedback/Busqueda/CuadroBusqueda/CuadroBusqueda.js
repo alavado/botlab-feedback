27bef2d477421cd4a34b2ced61f48a252aedd0c7
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-05-15 23:30:54 -0400

    parse codigo en canvas

--- src/components/Feedback/Busqueda/CuadroBusqueda/CuadroBusqueda.js
+++ src/components/Feedback/Busqueda/CuadroBusqueda/CuadroBusqueda.js
@@ -6,6 +6,27 @@ import Icon from '@iconify/react'
 import { useDispatch } from 'react-redux'
 import { limpiaBusqueda } from '../../../../redux/ducks/busqueda'
 
+const retrieveImageFromClipboardAsBlob = (pasteEvent, callback) => {
+	if (pasteEvent.clipboardData === false) {
+    if (typeof(callback) === "function") {
+        callback(undefined)
+    }
+  }
+  const items = pasteEvent.clipboardData.items
+  if (items === undefined){
+    if (typeof(callback) == "function") {
+        callback(undefined)
+    }
+  }
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].type.indexOf("image") === -1) {
+      continue
+    }
+    const blob = items[i].getAsFile()
+    callback(blob)
+  }
+}
+
 const CuadroBusqueda = () => {
 
   const inputRef = useRef()
@@ -18,6 +39,64 @@ const CuadroBusqueda = () => {
     dispatch(limpiaBusqueda())
   }, [dispatch])
 
+  useEffect(() => {
+    inputRef.current.addEventListener('paste',  e => {
+      retrieveImageFromClipboardAsBlob(e, imageBlob => {
+        if (imageBlob) {
+          var canvas = document.getElementById("mycanvas")
+          var ctx = canvas.getContext('2d')
+          var img = new Image()
+          img.onload = function() {
+            canvas.width = this.width
+            canvas.height = this.height
+            ctx.drawImage(img, 0, 0)
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+            const data = imageData.data
+            const grises = Array(data.length / 4).fill(0)
+            for (let i = 0; i < data.length; i += 4) {
+              const promedio = Math.floor((data[i] + data[i + 1] + data[i + 2]) / 3)
+              grises[i / 4] = promedio > 200 ? 255 : 0
+            }
+            const dx = Array(data.length / 4).fill(0)
+            for (let i = 0; i < dx.length; i++) {
+              dx[i] = grises[i + 1] - grises[i]
+            }
+            const bordes = Array(data.length / 4).fill(0)
+            bordes[0] = false
+            for (let i = 1; i < bordes.length; i++) {
+              bordes[i] = !bordes[i - 1] && (Math.sign(dx[i + 1]) !== Math.sign(dx[i]))
+              data[4 * i] = data[4 * i + 1] = data[4 * i + 2] = bordes[i] * 255
+              data[4 * i + 3] = 255
+            }
+            const proporciones = bordes.map(v => v ? '1' : '0').join('').split('1').map(v => v.length)
+            const largoPatron = 5
+            const delimitadores = proporciones
+              .map((v, i) => {
+                return i < (proporciones.length - largoPatron) && (
+                  v / proporciones[i + 1] === 1 &&
+                  Math.floor(proporciones[i + 2] / v) === 3 &&
+                  v / proporciones[i + 3] === 1 &&
+                  v / proporciones[i + 4] === 1
+                ) ? i : false
+              })
+              .filter(v => v)
+            if (delimitadores) {
+              const sliceDatos = proporciones.slice(delimitadores[0] + largoPatron, delimitadores[1])
+              const tamañoBit = proporciones[delimitadores[0]] * 1.33
+              const binario = sliceDatos
+                .map((v, i) => Array(Math.floor(v / tamañoBit)).fill(Number(i % 2 > 0)))
+                .flat()
+                .join('')
+              console.log(parseInt(binario, 2))
+            }
+            ctx.putImageData(imageData, 0, 0)
+          }
+          img.src = window.URL.createObjectURL(imageBlob)
+        }
+      })
+    }, false)
+  }, [])
+
   const buscar = e => {
     e.preventDefault()
     history.push(`/busqueda/${termino}`)
@@ -25,6 +104,7 @@ const CuadroBusqueda = () => {
 
   return (
     <div className="CuadroBusqueda">
+      <canvas style={{ border: '1px solid grey' }} id="mycanvas" />
       <form
         className="CuadroBusqueda__formulario"
         onSubmit={buscar}
