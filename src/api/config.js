264bdde890e7994da68bba47d51ac8daffd68901
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-05-24 20:16:06 -0400

    corrige url qa

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 export const API_ROOT = window.location.href.indexOf('dev') > 0
   ? 'https://api.dev.botlab.cl'
   : window.location.href.indexOf('qa') > 0
-    ? 'https://api.dev.botlab.cl' :
+    ? 'https://api.qa.botlab.cl' :
       window.location.href.indexOf('localhost') > 0
         ? 'https://api.qa.botlab.cl'
         : 'https://api.botlab.cl'
