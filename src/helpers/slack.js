980016dd84845b351cb1c4aa2e66c801d7fafb4a
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-08-05 12:35:55 -0400

    corrige flujo de reporte para que reporten a la velocidad de jperez

--- src/helpers/slack.js
+++ src/helpers/slack.js
@@ -46,32 +46,9 @@ export const reportarASlack = async (usuario, cuenta, tipo, descripcion) => {
     	}
     ],
   }
-  const fieldsPostData = new FormData()
-  fieldsPostData.append('token', process.env.REACT_APP_OAUTH2_TOKEN)
-  fieldsPostData.append('channel', process.env.REACT_APP_SLACK_CHANNEL_ID)
-  fieldsPostData.append('text', data.text)
-  fieldsPostData.append('unfurl_links', false)
-  fieldsPostData.append('blocks', JSON.stringify(data.blocks))
-  const fieldsData = await axios({
-    method: 'post',
-    url: 'https://slack.com/api/chat.postMessage',
-    data: fieldsPostData
-  })
-  const fieldsTimestamp = fieldsData.data.message.ts
 
   const nodoContenedor = document.getElementsByClassName('Feedback__contenedor_central')[0]
   const blobFB = await toBlob(nodoContenedor, { width: nodoContenedor.scrollWidth, height: nodoContenedor.scrollHeight })
-  const formData = new FormData()
-  formData.append('token', process.env.REACT_APP_OAUTH2_TOKEN)
-  formData.append('channels', process.env.REACT_APP_SLACK_CHANNEL_ID)
-  formData.append('file', blobFB)
-  formData.append('thread_ts', fieldsTimestamp)
-  await axios({
-    method: 'post',
-    url: process.env.REACT_APP_SLACK_FILE_UPLOAD_URL,
-    data: formData,
-    headers: { "Content-Type": "multipart/form-data" }
-  })
 
   const nodoContenedorMensajes = document.getElementsByClassName('CelularWhatsapp__contenedor_mensajes')[0]
   document.querySelectorAll('.CelularWhatsapp__contenedor_conversacion:not(.CelularWhatsapp__contenedor_conversacion--seleccionada)').forEach(nodo => {
@@ -85,6 +62,30 @@ export const reportarASlack = async (usuario, cuenta, tipo, descripcion) => {
   nodoContenedorMensajes.style.maxHeight = prevMaxHeight
   document.querySelectorAll('.CelularWhatsapp__contenedor_conversacion').forEach(nodo => {
     nodo.style.display = 'block'
+  })
+
+  const fieldsPostData = new FormData()
+  fieldsPostData.append('token', process.env.REACT_APP_OAUTH2_TOKEN)
+  fieldsPostData.append('channel', process.env.REACT_APP_SLACK_CHANNEL_ID)
+  fieldsPostData.append('text', data.text)
+  fieldsPostData.append('unfurl_links', false)
+  fieldsPostData.append('blocks', JSON.stringify(data.blocks))
+  const fieldsData = await axios({
+    method: 'post',
+    url: 'https://slack.com/api/chat.postMessage',
+    data: fieldsPostData
+  })
+  const fieldsTimestamp = fieldsData.data.message.ts
+  const formData = new FormData()
+  formData.append('token', process.env.REACT_APP_OAUTH2_TOKEN)
+  formData.append('channels', process.env.REACT_APP_SLACK_CHANNEL_ID)
+  formData.append('file', blobFB)
+  formData.append('thread_ts', fieldsTimestamp)
+  await axios({
+    method: 'post',
+    url: process.env.REACT_APP_SLACK_FILE_UPLOAD_URL,
+    data: formData,
+    headers: { "Content-Type": "multipart/form-data" }
   })
   formData.append('file', blob)
   await axios({
