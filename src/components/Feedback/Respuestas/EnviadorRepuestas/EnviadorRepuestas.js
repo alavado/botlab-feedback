22449dbf723a08b275403e0c813bfce14ce6f6d2
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-09-23 16:02:07 -0300

    conexion ok ahora si, pero sin validacion de errores

--- src/components/Feedback/Respuestas/EnviadorRepuestas/EnviadorRepuestas.js
+++ src/components/Feedback/Respuestas/EnviadorRepuestas/EnviadorRepuestas.js
@@ -3,6 +3,7 @@ import { createPortal } from 'react-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { desactivaEnviador } from '../../../../redux/ducks/enviador'
 import iconoContacto from '@iconify/icons-mdi/send'
+import iconoCerrar from '@iconify/icons-mdi/close'
 import { useMutation, useQuery } from 'react-query'
 import './EnviadorRepuestas.css'
 import { consultarMapping, crearEncuestas } from '../../../../api/endpoints'
@@ -12,20 +13,31 @@ import { ESQUEMA_OSCURO } from '../../../../redux/ducks/opciones'
 import { parse as parseCSV } from 'papaparse'
 import { some } from 'lodash'
 import { normalizar } from '../../../../redux/ducks/respuestas'
-import { format } from 'date-fns'
+import { format, parseISO } from 'date-fns'
 
 const obtenerTipoInput = header => {
   switch (header.type) {
     case 'text':
       return 'text'
     case 'datetime':
-      if (header.display_name.toLowerCase().includes('hora')) {
+      if (header.display_name.toLowerCase().startsWith('hora')) {
         return 'time'
       }
       return 'date'
     default:
       return header.type
   }
+}
+
+const formatearCampo = (header, valor) => {
+  if (header.display_name === 'FONO') {
+    return valor.replace(/[^0-9]/g, '')
+  }
+  else if (obtenerTipoInput(header) === 'date') {
+    console.log(valor)
+    return format(parseISO(valor), 'yyyy-MM-dd')
+  }
+  return valor
 }
 
 const EnviadorRepuestas = ({ idEncuesta }) => {
@@ -47,13 +59,14 @@ const EnviadorRepuestas = ({ idEncuesta }) => {
 
   const enviarEncuestas = e => {
     e.preventDefault()
-    const ahora = format(Date.now(), 'yyyy-MM-dd HH:ss')
+    const ahora = format(Date.now(), 'yyyy-MM-dd HH:mm:ss')
     const datos = filas.map(f => headersMenosConsultaConfirmacion.reduce((obj, h, i) => (
       {
         ...obj,
-        [h.display_name]: f[i]
+        [h.display_name]: formatearCampo(h, f[i])
       }
     ), { 'Consulta confirmación': ahora }))
+    console.log(datos)
     mutate({ idEncuesta, datos })
   }
 
@@ -73,7 +86,9 @@ const EnviadorRepuestas = ({ idEncuesta }) => {
           ...filas,
           ...data.map(fila => {
             const valores = headersMenosConsultaConfirmacion.map(h => fila[normalizar(h.display_name)] || '')
-            return some(valores) ? valores : null
+            return some(valores)
+              ? valores
+              : null
           }).filter(x => x)
         ])
       }
@@ -82,7 +97,7 @@ const EnviadorRepuestas = ({ idEncuesta }) => {
   }
 
   const abrirDialogoArchivo = () => document.getElementById('csv-enviador').click()
-  const agregarFila = () => setFilas([...filas, [...headersMenosConsultaConfirmacion].fill('')])
+  const agregarFilaVacía = () => setFilas([...filas, [...headersMenosConsultaConfirmacion].fill('')])
 
   return createPortal(
     <div
@@ -98,12 +113,19 @@ const EnviadorRepuestas = ({ idEncuesta }) => {
             className="EnviadorRespuestas__contenedor"
             onClick={e => e.stopPropagation()}
           >
+            <button
+              className="EnviadorRespuestas__boton_cerrar"
+              onClick={() => dispatch(desactivaEnviador())}
+              title="Cerrar"
+            >
+              <InlineIcon icon={iconoCerrar} />
+            </button>
             <div className="EnviadorRespuestas__superior">
               <h1>Contacto de usuarios</h1>
-              <div>
-                <button onClick={agregarFila}>Agregar fila</button>
-                <button onClick={abrirDialogoArchivo}>Cargar archivo</button>
-                <button onClick={() => setFilas([])}>Limpiar tabla</button>
+              <div className="EnviadorRespuestas__superior_acciones">
+                <button className="EnviadorRespuestas__boton_accion" onClick={agregarFilaVacía}>Agregar fila</button>
+                <button className="EnviadorRespuestas__boton_accion" onClick={abrirDialogoArchivo}>Cargar archivo</button>
+                <button className="EnviadorRespuestas__boton_accion" onClick={() => setFilas([])}>Limpiar tabla</button>
                 <input
                   style={{ display: 'none' }}
                   id="csv-enviador"
@@ -151,7 +173,7 @@ const EnviadorRepuestas = ({ idEncuesta }) => {
                         ))
                       : <tr>
                           <td className="EnviadorRespuestas__mensaje_sin_datos" colSpan={data.data.data.length}>
-                          Puedes agregar usuarios desde un <span onClick={abrirDialogoArchivo}>archivo</span> CSV o <span onClick={agregarFila}>manualmente</span>
+                          Puedes agregar usuarios desde un <span onClick={abrirDialogoArchivo}>archivo</span> CSV o <span onClick={agregarFilaVacía}>manualmente</span>
                           </td>
                         </tr>
                     }
