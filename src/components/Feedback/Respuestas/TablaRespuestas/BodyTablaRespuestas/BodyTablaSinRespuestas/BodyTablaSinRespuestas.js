09c3f62cd8a1728cb462e262095b7d26f386264b
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-10-12 10:19:37 -0300

    agrega hora de inicio obtenida desde api

--- src/components/Feedback/Respuestas/TablaRespuestas/BodyTablaRespuestas/BodyTablaSinRespuestas/BodyTablaSinRespuestas.js
+++ src/components/Feedback/Respuestas/TablaRespuestas/BodyTablaRespuestas/BodyTablaSinRespuestas/BodyTablaSinRespuestas.js
@@ -7,12 +7,20 @@ import { isToday } from 'date-fns'
 const BodyTablaSinRespuestas = () => {
 
   const { fechaTermino } = useSelector(state => state.respuestas)
+  const { tipos, idEncuestaSeleccionada } = useSelector(state => state.encuestas)
+
+  if (!idEncuestaSeleccionada) {
+    return null
+  }
+
+  const encuestaSeleccionada = tipos.find(({ id }) => id === idEncuestaSeleccionada)
+  const horaInicio = encuestaSeleccionada.integrations?.[0].start_time
 
   return (
     <div className="BodyTablaSinRespuestas">
       <Icon className="BodyTablaSinRespuestas__icono" icon={iconoSinRepuestas} />
       <p>No se encontraron respuestas</p>
-      {isToday(fechaTermino) && <p>Las interacciones con pacientes<br />comienzan a las <strong>8:30</strong></p> }
+      {isToday(fechaTermino) && horaInicio && <p>Las interacciones con pacientes<br />comienzan a las <strong>{horaInicio}</strong></p> }
     </div>
   )
 }
