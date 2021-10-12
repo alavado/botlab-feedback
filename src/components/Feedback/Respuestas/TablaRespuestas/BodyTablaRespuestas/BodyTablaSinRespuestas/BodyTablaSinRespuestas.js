f2df0bf8e900c90e66e86b3455aef637e2af76ad
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-10-12 10:20:04 -0300

    refactor presentacion

--- src/components/Feedback/Respuestas/TablaRespuestas/BodyTablaRespuestas/BodyTablaSinRespuestas/BodyTablaSinRespuestas.js
+++ src/components/Feedback/Respuestas/TablaRespuestas/BodyTablaRespuestas/BodyTablaSinRespuestas/BodyTablaSinRespuestas.js
@@ -1,8 +1,10 @@
 import Icon from '@iconify/react'
-import iconoSinRepuestas from '@iconify/icons-mdi/checkbox-blank-off-outline'
+import iconoSinRepuestas from '@iconify/icons-mdi/robot-confused'
+import iconoTodaviaNo from '@iconify/icons-mdi/robot'
 import './BodyTablaSinRespuestas.css'
 import { useSelector } from 'react-redux'
-import { isToday } from 'date-fns'
+import { isAfter } from 'date-fns'
+import endOfYesterday from 'date-fns/endOfYesterday'
 
 const BodyTablaSinRespuestas = () => {
 
@@ -15,12 +17,15 @@ const BodyTablaSinRespuestas = () => {
 
   const encuestaSeleccionada = tipos.find(({ id }) => id === idEncuestaSeleccionada)
   const horaInicio = encuestaSeleccionada.integrations?.[0].start_time
+  const todaviaNo = horaInicio && isAfter(fechaTermino, endOfYesterday())
 
   return (
     <div className="BodyTablaSinRespuestas">
-      <Icon className="BodyTablaSinRespuestas__icono" icon={iconoSinRepuestas} />
-      <p>No se encontraron respuestas</p>
-      {isToday(fechaTermino) && horaInicio && <p>Las interacciones con pacientes<br />comienzan a las <strong>{horaInicio}</strong></p> }
+      <Icon className="BodyTablaSinRespuestas__icono" icon={todaviaNo ? iconoTodaviaNo : iconoSinRepuestas} />
+      {todaviaNo
+        ? <p>Las interacciones con pacientes<br />comienzan a las <strong>{horaInicio}</strong></p>
+        : <p>No se encontraron respuestas</p>
+      }
     </div>
   )
 }
