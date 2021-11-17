688d19f813ab623a645947b61d3b6d41e23ee12d
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-11-17 17:15:57 -0300

    respuesta calculada para ayv dental 2 dias antes

--- src/helpers/tagsCalculados.js
+++ src/helpers/tagsCalculados.js
@@ -129,6 +129,7 @@ export const obtenerTagsCalculados = idEncuesta => {
       case Number(process.env.REACT_APP_ID_POLL_SANTA_BLANCA_RECONFIRMACION):
       case Number(process.env.REACT_APP_ID_POLL_MEDISIS):
       case Number(process.env.REACT_APP_ID_POLL_AYVDENTAL):
+      case Number(process.env.REACT_APP_ID_POLL_AYVDENTAL_2):
       case Number(process.env.REACT_APP_ID_POLL_DENTALONE):
       case Number(process.env.REACT_APP_ID_POLL_3DENTONCE16):
       case Number(process.env.REACT_APP_ID_POLL_NATURALDENT):
