cb9ce2ab18fcdb9975e7a15502f9cad0d1301af6
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-12-06 13:25:45 -0300

    remueve alerta odontos

--- src/components/Feedback/BarraSuperior/AlertaPilotos/AlertaPilotos.js
+++ src/components/Feedback/BarraSuperior/AlertaPilotos/AlertaPilotos.js
@@ -12,10 +12,6 @@ const usuariosPiloto = [
     exp: '2021-11-13 00:00:00'
   },
   {
-    nombre: 'Odontos',
-    exp: '2021-12-03 00:00:00'
-  },
-  {
     nombre: 'Ventus',
     exp: '2021-12-18 00:00:00'
   },
