04b456f212f427482285264730073a74e8e56384
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-12-20 15:55:34 -0300

    config menta

--- src/helpers/tagsCalculados.js
+++ src/helpers/tagsCalculados.js
@@ -169,6 +169,7 @@ export const obtenerTagsCalculados = idEncuesta => {
       case Number(process.env.REACT_APP_ID_POLL_DENTALSKI):
       case Number(process.env.REACT_APP_ID_POLL_BEHAPPY):
       case Number(process.env.REACT_APP_ID_POLL_CRD):
+      case Number(process.env.REACT_APP_ID_POLL_MENTA):
         return juntarConfirmaYReagenda(0, 104)
       
       case Number(process.env.REACT_APP_ID_POLL_DENTALSTUDIO):
