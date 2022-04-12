c17f5abc6f83c199437c79676c197962ffbd9554
Alejandro Lavado <alejandro.lvd@gmail.com>
2022-04-11 20:56:37 -0400

    config sonrisa imperecedera

--- src/components/Feedback/BarraSuperior/AlertaPilotos/AlertaPilotos.js
+++ src/components/Feedback/BarraSuperior/AlertaPilotos/AlertaPilotos.js
@@ -23,6 +23,10 @@ const usuariosPiloto = [
     nombre: 'AltaEspecialidadDental',
     exp: '2022-05-01 00:00:00'
   },
+  {
+    nombre: '4EverSmile',
+    exp: '2022-04-29 00:00:00'
+  },
 ]
 
 const AlertaPilotos = () => {
--- src/helpers/tagsCalculados.js
+++ src/helpers/tagsCalculados.js
@@ -237,6 +237,7 @@ export const obtenerTagsCalculados = idEncuesta => {
       case Number(process.env.REACT_APP_ID_POLL_CSMI):
       case Number(process.env.REACT_APP_ID_POLL_ALTA_ESPECIALIDAD_DENTAL):
       case Number(process.env.REACT_APP_ID_POLL_BUKAL):
+      case Number(process.env.REACT_APP_ID_POLL_4EVER_SMILE):
         return juntarConfirmaYReagenda(0, 104)
       
       case Number(process.env.REACT_APP_ID_POLL_DENTALSTUDIO):
