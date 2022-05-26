b595281d07d0b107a1625ae265c366cae8ca4ab0
Alejandro Lavado <alejandro.lvd@gmail.com>
2022-05-26 19:22:03 -0400

    reordena los videos de tutoriales según orden jorge

--- src/components/Feedback/Tutoriales/Tutoriales.js
+++ src/components/Feedback/Tutoriales/Tutoriales.js
@@ -4,10 +4,6 @@ import './Tutoriales.css'
 
 const videos = [
   {
-    titulo: 'Confirmaciones por fecha',
-    link: 'https://player.vimeo.com/video/707555525?h=d41f18602a'
-  },
-  {
     titulo: 'Filtros simples',
     link: 'https://player.vimeo.com/video/707563556?h=202cf81392'
   },
@@ -16,16 +12,20 @@ const videos = [
     link: 'https://player.vimeo.com/video/710118003?h=c24e9a7220'
   },
   {
-    titulo: 'Audios',
-    link: 'https://player.vimeo.com/video/707567810?h=df51a47072'
+    titulo: 'Confirmaciones por fecha',
+    link: 'https://player.vimeo.com/video/707555525?h=d41f18602a'
+  },
+  {
+    titulo: 'Reportar un problema',
+    link: 'https://player.vimeo.com/video/707572633?h=dbb41fa1ba'
   },
   {
     titulo: 'Agregar comentarios a un chat',
     link: 'https://player.vimeo.com/video/707569196?h=222e75a7d0'
   },
   {
-    titulo: 'Reportar problema',
-    link: 'https://player.vimeo.com/video/707572633?h=dbb41fa1ba'
+    titulo: 'Audios',
+    link: 'https://player.vimeo.com/video/707567810?h=df51a47072'
   },
   {
     titulo: 'Alertas',
