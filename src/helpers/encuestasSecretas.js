6315e6619d43add9a4956470faa6c893c678e03b
Alejandro Lavado <alejandro.lvd@gmail.com>
2022-07-12 17:11:41 -0400

    refactor de las encuestas ocultas para permitir acceso a todas las cuentas cero botlab, menos camilo

--- src/helpers/encuestasSecretas.js
+++ src/helpers/encuestasSecretas.js
@@ -1,20 +1,8 @@
 export const obtenerTiposEncuestasVisibles = (cuenta, tipos) => {
-  if (cuenta === 'mega_admin_cero') {
+  const cuentaLC = cuenta?.toLowerCase() || ''
+  if (cuentaLC.endsWith('_cero') || cuentaLC.endsWith('_botlab')) {
     return tipos
   }
   let tiposEncuestas = tipos?.slice() || []
-  const cuentaLC = cuenta?.toLowerCase() || ''
-  if (cuentaLC !== 'sanasalud_botlab') {
-    tiposEncuestas = tiposEncuestas.filter(t => t.id !== 233)
-  }
-  if (cuentaLC !== 'falp_cero') {
-    tiposEncuestas = tiposEncuestas.filter(t => t.id !== 374)
-  }
-  if (cuentaLC !== 'alemana_cero') {
-    tiposEncuestas = tiposEncuestas.filter(t => t.id !== 457)
-  }
-  if (cuentaLC !== 'everest_cero') {
-    tiposEncuestas = tiposEncuestas.filter(t => t.id !== 577)
-  }
-  return tiposEncuestas
+  return tiposEncuestas.filter(t => ![233, 374, 457, 577].includes(t.id))
 }
