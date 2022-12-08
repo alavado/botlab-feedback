266f74c61033d5795d3f84557f90b7d6026120c6
Alejandro Lavado <alejandro.lvd@gmail.com>
2022-12-08 14:07:49 -0300

    state para estados de cajon lateral

--- src/redux/ducks/search.ts
+++ src/redux/ducks/search.ts
@@ -1,7 +1,8 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface SearchState {
-  term: string
+  term: string,
+  drawerVisible: boolean,
 }
 
 const searchSlice = createSlice({
@@ -13,11 +14,19 @@ const searchSlice = createSlice({
     setTerm(state, action: PayloadAction<string>) {
       state.term = action.payload
     },
+    showDrawer(state) {
+      state.drawerVisible = true
+    },
+    hideDrawer(state) {
+      state.drawerVisible = false
+    },
   }
 })
 
 export const {
   setTerm,
+  showDrawer,
+  hideDrawer,
 } = searchSlice.actions
 
 export default searchSlice.reducer
