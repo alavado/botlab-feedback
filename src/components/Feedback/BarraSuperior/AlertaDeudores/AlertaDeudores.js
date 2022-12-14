35880dcc355f1a1e3533a7a888240073cb15d3db
Alejandro Lavado <alejandro.lvd@gmail.com>
2022-12-14 07:52:52 -0300

    actualiza lista de deudores

--- src/components/Feedback/BarraSuperior/AlertaDeudores/AlertaDeudores.js
+++ src/components/Feedback/BarraSuperior/AlertaDeudores/AlertaDeudores.js
@@ -1,39 +1,57 @@
-import { useSelector } from 'react-redux'
-import './AlertaDeudores.css'
+import { useSelector } from "react-redux";
+import "./AlertaDeudores.css";
 
 const usuariosDeudores = [
   // 'BiobioSalud'
-  'Sanasalud',
-  'Everest',
-  'LeCiel',
-  'Maitenes',
-  'SantaBlanca',
-  'Norden',
-  'Santis',
-  'SonrieArica',
-  'ImagenSalud',
-  'RoaDent',
-  'MAZ',
-  'CerroGrande',
-  'YohananTherapeutes',
-]
+
+  "AltoTobalaba",
+  "Redental",
+  "EzioChiappe",
+  "BeHappy",
+  "Facelab",
+  "3DentOnce16",
+  "AltosDelValle",
+  "Falcon",
+  "Tabilo",
+  "Tobalaba",
+  "CATH",
+  "RAO",
+  "Made",
+  "BDental",
+  "Cesmed",
+  "YohananTherapeutes",
+  "AyVDental",
+  "CTMelipilla",
+  "RoaDent",
+  "MAZ",
+  "SonrieArica",
+  "CerroGrande",
+  "Santis",
+  "OYEDental",
+  "Norden",
+  "SantaBlanca",
+  "Everest",
+  "LeCiel",
+  "Sanasalud",
+  "Maitenes",
+];
 
 const AlertaDeudores = () => {
-
-  const { nombreUsuario } = useSelector(state => state.login)
-  const usuario = usuariosDeudores.find(u => u === nombreUsuario)
+  const { nombreUsuario } = useSelector((state) => state.login);
+  const usuario = usuariosDeudores.find((u) => u === nombreUsuario);
 
   if (!usuario) {
-    return null
+    return null;
   }
 
-  const correoPrincipal = `finanzas@cero.ai`
+  const correoPrincipal = `finanzas@cero.ai`;
 
   return (
     <div className="AlertaDeudores">
-      Tienes facturas vencidas en espera de pago. Regulariza tu deuda comunicándote al +56942773233 o en {correoPrincipal}
+      Tienes facturas vencidas en espera de pago. Regulariza tu deuda
+      comunicándote al +56942773233 o en {correoPrincipal}
     </div>
-  )
-}
+  );
+};
 
-export default AlertaDeudores
+export default AlertaDeudores;
