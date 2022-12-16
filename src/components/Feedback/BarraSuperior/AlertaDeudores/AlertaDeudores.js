6b6e35e10d5606052debe5ba8b5d287f99b60d98
Alejandro Lavado <alejandro.lvd@gmail.com>
2022-12-16 12:11:14 -0300

    remueve banner rao

--- src/components/Feedback/BarraSuperior/AlertaDeudores/AlertaDeudores.js
+++ src/components/Feedback/BarraSuperior/AlertaDeudores/AlertaDeudores.js
@@ -1,57 +1,56 @@
-import { useSelector } from "react-redux";
-import "./AlertaDeudores.css";
+import { useSelector } from 'react-redux'
+import './AlertaDeudores.css'
 
 const usuariosDeudores = [
   // 'BiobioSalud'
 
-  "AltoTobalaba",
-  "Redental",
-  "EzioChiappe",
-  "BeHappy",
-  "Facelab",
-  "3DentOnce16",
-  "AltosDelValle",
-  "Falcon",
-  "Tabilo",
-  "Tobalaba",
-  "CATH",
-  "RAO",
-  "Made",
-  "BDental",
-  "Cesmed",
-  "YohananTherapeutes",
-  "AyVDental",
-  "RoaDent",
-  "MAZ",
-  "SonrieArica",
-  "CerroGrande",
-  "Santis",
-  "OYEDental",
-  "Norden",
-  "SantaBlanca",
-  "Everest",
-  "LeCiel",
-  "Sanasalud",
-  "Maitenes",
-  "Medisis",
-];
+  'AltoTobalaba',
+  'Redental',
+  'EzioChiappe',
+  'BeHappy',
+  'Facelab',
+  '3DentOnce16',
+  'AltosDelValle',
+  'Falcon',
+  'Tabilo',
+  'Tobalaba',
+  'CATH',
+  'Made',
+  'BDental',
+  'Cesmed',
+  'YohananTherapeutes',
+  'AyVDental',
+  'RoaDent',
+  'MAZ',
+  'SonrieArica',
+  'CerroGrande',
+  'Santis',
+  'OYEDental',
+  'Norden',
+  'SantaBlanca',
+  'Everest',
+  'LeCiel',
+  'Sanasalud',
+  'Maitenes',
+  'Medisis',
+]
 
 const AlertaDeudores = () => {
-  const { nombreUsuario } = useSelector((state) => state.login);
-  const usuario = usuariosDeudores.find((u) => u === nombreUsuario);
+  const { nombreUsuario } = useSelector((state) => state.login)
+  const usuario = usuariosDeudores.find((u) => u === nombreUsuario)
 
   if (!usuario) {
-    return null;
+    return null
   }
 
-  const correoPrincipal = `finanzas@cero.ai`;
+  const correoPrincipal = `finanzas@cero.ai`
 
   return (
     <div className="AlertaDeudores">
       Tienes facturas vencidas en espera de pago. Regulariza tu deuda
       comunicándote al +56942773233 o en {correoPrincipal}
     </div>
-  );
-};
+  )
+}
 
-export default AlertaDeudores;
+export default AlertaDeudores
