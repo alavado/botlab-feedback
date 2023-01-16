b791e133b4b8073cf1f1ff4b82c844e1266709fd
Alejandro Lavado <alejandro.lvd@gmail.com>
2023-01-16 11:18:30 -0300

    formateo de nombre de encuesta en título de selector de encuesta

--- src/components/Feedback/BarraSuperior/SelectorEncuesta/SelectorEncuesta.js
+++ src/components/Feedback/BarraSuperior/SelectorEncuesta/SelectorEncuesta.js
@@ -6,83 +6,108 @@ import { guardaHeadersEncuesta } from '../../../../redux/ducks/encuestas'
 import PopupEncuestas from './PopupEncuestas'
 import Loader from '../../../Loader'
 import './SelectorEncuesta.css'
-import { actualizaRespuestas, agregaFiltro, limpiaFiltros, limpiaRespuestas } from '../../../../redux/ducks/respuestas'
+import {
+  actualizaRespuestas,
+  agregaFiltro,
+  limpiaFiltros,
+  limpiaRespuestas,
+} from '../../../../redux/ducks/respuestas'
 import { guardaIdEncuesta } from '../../../../redux/ducks/opciones'
 import { useParams, useRouteMatch } from 'react-router-dom'
 import classNames from 'classnames'
 import Scrambler from '../../../Scrambler/Scrambler'
 import { obtenerTiposEncuestasVisibles } from '../../../../helpers/encuestasSecretas'
+import { formatearNombreEncuesta } from '../../../../helpers/respuestas'
 
 const SelectorEncuesta = () => {
-
-  const { tipos, idEncuestaSeleccionada } = useSelector(state => state.encuestas)
-  const { idEncuestaGuardada } = useSelector(state => state.opciones)
-  const { cuenta } = useSelector(state => state.login)
+  const { tipos, idEncuestaSeleccionada } = useSelector(
+    (state) => state.encuestas
+  )
+  const { idEncuestaGuardada } = useSelector((state) => state.opciones)
+  const { cuenta, nombreUsuario } = useSelector((state) => state.login)
   const [cargandoEncuesta, setCargandoEncuesta] = useState(false)
   const [popupActivo, setPopupActivo] = useState(false)
-  const { indiceRespuestaSeleccionada, nombreEncuestaFiltrada } = useSelector(state => state.respuestas)
+  const { indiceRespuestaSeleccionada, nombreEncuestaFiltrada } = useSelector(
+    (state) => state.respuestas
+  )
   const { idEncuesta: idEncuestaRuta } = useParams()
   const { path } = useRouteMatch()
   const dispatch = useDispatch()
 
-  const verEncuesta = useCallback(async id => {
-    setCargandoEncuesta(true)
-    if (`${id}`.startsWith('filtro')) {
-      setCargandoEncuesta(false)
-      const [_, header, texto, idEncuesta, titulo] = id.split('|')
-      dispatch(agregaFiltro({
-        busqueda: texto,
-        nombreHeader: header,
-        textoHeader: header,
-        idEncuesta: Number(idEncuesta),
-        opciones: {
-          filtroImplicito: true,
-          titulo
-        }
-      }))
-      return
-    }
-    if (id === idEncuestaSeleccionada) {
-      setCargandoEncuesta(false)
-      dispatch(limpiaFiltros())
-      return
-    }
-    try {
-      dispatch(limpiaRespuestas())
-      dispatch(guardaIdEncuesta(id))
-      dispatch(limpiaFiltros())
-      const data = await headersAPI(id)
-      dispatch(guardaHeadersEncuesta({ id, data }))
-      dispatch(actualizaRespuestas())
-      setCargandoEncuesta(false)
-    } catch (e) {
-      console.error('un error', e)
-    }
-  }, [dispatch, idEncuestaSeleccionada])
+  const verEncuesta = useCallback(
+    async (id) => {
+      setCargandoEncuesta(true)
+      if (`${id}`.startsWith('filtro')) {
+        setCargandoEncuesta(false)
+        const [_, header, texto, idEncuesta, titulo] = id.split('|')
+        dispatch(
+          agregaFiltro({
+            busqueda: texto,
+            nombreHeader: header,
+            textoHeader: header,
+            idEncuesta: Number(idEncuesta),
+            opciones: {
+              filtroImplicito: true,
+              titulo,
+            },
+          })
+        )
+        return
+      }
+      if (id === idEncuestaSeleccionada) {
+        setCargandoEncuesta(false)
+        dispatch(limpiaFiltros())
+        return
+      }
+      try {
+        dispatch(limpiaRespuestas())
+        dispatch(guardaIdEncuesta(id))
+        dispatch(limpiaFiltros())
+        const data = await headersAPI(id)
+        dispatch(guardaHeadersEncuesta({ id, data }))
+        dispatch(actualizaRespuestas())
+        setCargandoEncuesta(false)
+      } catch (e) {
+        console.error('un error', e)
+      }
+    },
+    [dispatch, idEncuestaSeleccionada]
+  )
 
   useEffect(() => {
     let tiposEncuestas = obtenerTiposEncuestasVisibles(cuenta, tipos)
     if (tiposEncuestas && !idEncuestaSeleccionada) {
-      if (idEncuestaRuta && tiposEncuestas.find(t => t.id === Number(idEncuestaRuta))) {
+      if (
+        idEncuestaRuta &&
+        tiposEncuestas.find((t) => t.id === Number(idEncuestaRuta))
+      ) {
         verEncuesta(Number(idEncuestaRuta))
-      }
-      else if (tiposEncuestas.find(t => t.id === idEncuestaGuardada)) {
+      } else if (tiposEncuestas.find((t) => t.id === idEncuestaGuardada)) {
         verEncuesta(idEncuestaGuardada)
-      }
-      else {
+      } else {
         const id = tiposEncuestas[0]?.id
         if (id) {
           verEncuesta(id)
         }
       }
     }
-  }, [idEncuestaSeleccionada, idEncuestaGuardada, idEncuestaRuta, dispatch, tipos, verEncuesta, cuenta])
+  }, [
+    idEncuestaSeleccionada,
+    idEncuestaGuardada,
+    idEncuestaRuta,
+    dispatch,
+    tipos,
+    verEncuesta,
+    cuenta,
+  ])
 
   if (!idEncuestaSeleccionada) {
     return <Loader color="#6057f6" />
   }
 
-  const encuestaSeleccionada = tipos.find(t => t.id === idEncuestaSeleccionada)
+  const encuestaSeleccionada = tipos.find(
+    (t) => t.id === idEncuestaSeleccionada
+  )
 
   if (!encuestaSeleccionada && !cargandoEncuesta) {
     verEncuesta(tipos[0].id)
@@ -97,39 +122,58 @@ const SelectorEncuesta = () => {
     <div
       className={classNames({
         SelectorEncuesta: true,
-        'SelectorEncuesta--visible': path.indexOf('chat') < 0 || indiceRespuestaSeleccionada >= 0,
+        'SelectorEncuesta--visible':
+          path.indexOf('chat') < 0 || indiceRespuestaSeleccionada >= 0,
         'SelectorEncuesta--interactivo': path.indexOf('chat') < 0,
-        'SelectorEncuesta--todas': path.indexOf('busqueda') >= 0
+        'SelectorEncuesta--todas': path.indexOf('busqueda') >= 0,
       })}
       onClick={() => setPopupActivo(path.indexOf('chat') < 0)}
     >
-      {['busqueda', 'uso', 'alertas', 'tutoriales', 'exportar'].some(x => path.indexOf(x) >= 0)
-        ? <>
+      {['busqueda', 'uso', 'alertas', 'tutoriales', 'exportar'].some(
+        (x) => path.indexOf(x) >= 0
+      ) ? (
+        <>
+          <Icon
+            className="SelectorEncuesta__icono_empresa"
+            icon="mdi:whatsapp"
+          />
+          <div className="SelectorEncuesta__nombre_encuesta">
+            Todos los servicios
+          </div>
+        </>
+      ) : (
+        <>
+          <Icon
+            className="SelectorEncuesta__icono_empresa"
+            icon={
+              nombreEncuestaFiltrada ? 'mdi:map-marker-radius' : 'mdi:whatsapp'
+            }
+            style={{
+              color: encuestaSeleccionada.enabled ? '#48BB78' : '#9f9eae',
+            }}
+          />
+          <div className="SelectorEncuesta__nombre_encuesta">
+            <Scrambler tipo="multi">
+              {nombreEncuestaFiltrada ||
+                formatearNombreEncuesta(
+                  nombreUsuario,
+                  encuestaSeleccionada.nombre
+                )}
+            </Scrambler>
+          </div>
+          {path.indexOf('chat') < 0 && (
             <Icon
-              className="SelectorEncuesta__icono_empresa"
-              icon="mdi:whatsapp"
+              className="SelectorEncuesta__icono_menu"
+              icon="mdi:chevron-down"
             />
-            <div className="SelectorEncuesta__nombre_encuesta">
-              Todos los servicios
-            </div>
-          </>
-        : <>
-            <Icon
-              className="SelectorEncuesta__icono_empresa"
-              icon={nombreEncuestaFiltrada ? 'mdi:map-marker-radius' : 'mdi:whatsapp'}
-              style={{ color: encuestaSeleccionada.enabled ? '#48BB78' : '#9f9eae' }}
-            />
-            <div className="SelectorEncuesta__nombre_encuesta">
-              <Scrambler tipo="multi">{nombreEncuestaFiltrada || encuestaSeleccionada.nombre}</Scrambler>
-            </div>
-            {path.indexOf('chat') < 0 && <Icon className="SelectorEncuesta__icono_menu" icon="mdi:chevron-down" />}
-            <PopupEncuestas
-              activo={popupActivo}
-              esconder={() => setPopupActivo(false)}
-              verEncuesta={verEncuesta}
-            />
-          </>
-      }
+          )}
+          <PopupEncuestas
+            activo={popupActivo}
+            esconder={() => setPopupActivo(false)}
+            verEncuesta={verEncuesta}
+          />
+        </>
+      )}
     </div>
   )
 }
