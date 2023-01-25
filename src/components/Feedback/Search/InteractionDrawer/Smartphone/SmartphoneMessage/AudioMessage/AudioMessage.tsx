f0da249326ed8ee4488b72d4545d1a8b470c3bb3
Alejandro Lavado <alejandro.lvd@gmail.com>
2023-01-25 18:50:13 -0300

    soporte provisorio para audios

--- src/components/Feedback/Search/InteractionDrawer/Smartphone/SmartphoneMessage/AudioMessage/AudioMessage.tsx
+++ src/components/Feedback/Search/InteractionDrawer/Smartphone/SmartphoneMessage/AudioMessage/AudioMessage.tsx
@@ -1,7 +1,17 @@
+import useAnswerMediaQuery from '../../../../../../../api/hooks/useAnswerMediaQuery'
 import './AudioMessage.css'
 
-const AudioMessage = () => {
-  return <div className="AudioMessage">AudioMessage</div>
+const AudioMessage = ({ answerId }: { answerId: number }) => {
+  const { data, isLoading } = useAnswerMediaQuery(answerId)
+
+  if (isLoading) {
+    return <p>cargando...</p>
+  }
+  return (
+    <div className="AudioMessage">
+      <audio className="MensajeWhatsapp__audio" src={data?.url} controls />
+    </div>
+  )
 }
 
 export default AudioMessage
--- src/components/Feedback/Search/InteractionDrawer/Smartphone/SmartphoneMessage/helpers.tsx
+++ src/components/Feedback/Search/InteractionDrawer/Smartphone/SmartphoneMessage/helpers.tsx
@@ -1,4 +1,5 @@
 import AttachmentMessage from './AttachmentMessage'
+import AudioMessage from './AudioMessage'
 import PictureMessage from './PictureMessage'
 import TextMessage from './TextMessage'
 
@@ -9,7 +10,11 @@ const hasAttachment = (message: String): Boolean => {
 }
 
 const isPicture = (message: String): Boolean => {
-  return message.indexOf('MEDIAIMAGEURL') >= 0
+  return message === 'MEDIAIMAGEURL'
+}
+
+const isAudio = (message: String): Boolean => {
+  return message === 'MEDIAAUDIOURL'
 }
 
 export const getMessageContentComponent = (
@@ -21,6 +26,9 @@ export const getMessageContentComponent = (
   }
   if (isPicture(message)) {
     return <PictureMessage answerId={messageId} />
+  }
+  if (isAudio(message)) {
+    return <AudioMessage answerId={messageId} />
   }
   return <TextMessage message={message} />
 }
