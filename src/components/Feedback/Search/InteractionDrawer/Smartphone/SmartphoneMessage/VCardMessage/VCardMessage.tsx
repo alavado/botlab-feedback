0dfd387f3d0180fe1ec034eb32b11317414d921d
Alejandro Lavado <alejandro.lvd@gmail.com>
2023-01-26 17:24:48 -0300

    implementacion para vcards

--- src/components/Feedback/Search/InteractionDrawer/Smartphone/SmartphoneMessage/VCardMessage/VCardMessage.tsx
+++ src/components/Feedback/Search/InteractionDrawer/Smartphone/SmartphoneMessage/VCardMessage/VCardMessage.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '@iconify/react'
 import useVCardQuery from '../../../../../../../api/hooks/useVCardQuery'
 import Loader from '../../../../../../Loader'
 import './VCardMessage.css'
@@ -9,7 +10,27 @@ const VCardMessage = ({ answerId }: { answerId: number }) => {
     return <Loader color="var(--color-principal)" />
   }
 
-  return <div className="VCardMessage">{data?.phone}</div>
+  return (
+    <div className="VCardMessage">
+      <div className="VCardMessage__top">
+        <Icon icon="mdi:user-circle" className="VCardMessage__avatar" />
+        <span className="VCardMessage__phone">{data?.phone}</span>
+      </div>
+      <div>
+        <a
+          href={`https://web.whatsapp.com/send/?phone=${data?.phone.replace(
+            /[^0-9]/g,
+            ''
+          )}`}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="VCardMessage__link_whatsapp"
+        >
+          Contactar por Whatsapp
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default VCardMessage
