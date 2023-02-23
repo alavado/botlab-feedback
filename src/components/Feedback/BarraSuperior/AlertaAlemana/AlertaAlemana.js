612a61fe39155360d83c4a64f268b5e065b319b2
Alejandro Lavado <alejandro.lvd@gmail.com>
2023-02-23 12:10:39 -0300

    agrega link a correo para alemana seguros

--- src/components/Feedback/BarraSuperior/AlertaAlemana/AlertaAlemana.js
+++ src/components/Feedback/BarraSuperior/AlertaAlemana/AlertaAlemana.js
@@ -11,7 +11,14 @@ const AlertaAlemana = () => {
   return (
     <div className="AlertaAlemana">
       Desde el 1 de marzo de 2023 este servicio ya no sigue vigente. Para
-      continuar con el servicio, comunícate con Felipe Rodríguez (fr@cero.ai)
+      continuar con el servicio, comunícate con Felipe Rodríguez (
+      <a
+        className="AlertaAlemana__link"
+        href={`mailto:fr@cero.ai?subject=Continuación%20Servicio%20Alemana%20Seguros`}
+      >
+        fr@cero.ai
+      </a>
+      )
     </div>
   )
 }
