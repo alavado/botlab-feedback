f5c13d65cd48145c7e4e13421969b3b9f1868007
Alejandro Lavado <alejandro.lvd@gmail.com>
2023-04-04 11:11:40 -0400

    remueve banner para yt

--- src/components/Feedback/PaymentDueBanner/debtors.ts
+++ src/components/Feedback/PaymentDueBanner/debtors.ts
@@ -6,35 +6,34 @@ type Debtor = {
 }
 
 const debtorsData = `
-  Medisis;2023-04-11
-  LasCruces;2023-04-11
-  CEOLA Vina;2023-04-11
-  3DentOnce16;2023-04-11
-  Falcon;2023-04-11
-  CEOLA Temuco;2023-04-11
-  Citymed;2023-04-11
-  YohananTherapeutes;2023-04-11
-  uandes;2023-04-11
-  CEOLA Concepcion;2023-04-11
-  COP;2023-04-11
-  Vitasalud;2023-04-11
-  Aquamed;2023-04-11
-  CTMelipilla;2023-04-11
-  Cegin;2023-04-11
-  Vitales;2023-04-11
-  UTC;2023-04-11
-  EzioChiappe;2023-04-11
-  Integral Linares;2023-04-11
-  2020;2023-04-11
-  BrisasSalud;2023-04-11
-  ClinicaAstra;2023-04-11
-  FARR;2023-04-11
-  SantaBlanca;2023-04-11
-  Everest;2023-04-11
-  FALP;2023-04-11
-  NucleoSalud;2023-04-11
-  Maitenes;2023-04-11
-  Sanasalud;2023-04-11
+  Medisis;              2023-04-11
+  LasCruces;            2023-04-11
+  CEOLA Vina;           2023-04-11
+  3DentOnce16;          2023-04-11
+  Falcon;               2023-04-11
+  CEOLA Temuco;         2023-04-11
+  Citymed;              2023-04-11
+  uandes;               2023-04-11
+  CEOLA Concepcion;     2023-04-11
+  COP;                  2023-04-11
+  Vitasalud;            2023-04-11
+  Aquamed;              2023-04-11
+  CTMelipilla;          2023-04-11
+  Cegin;                2023-04-11
+  Vitales;              2023-04-11
+  UTC;                  2023-04-11
+  EzioChiappe;          2023-04-11
+  Integral Linares;     2023-04-11
+  2020;                 2023-04-11
+  BrisasSalud;          2023-04-11
+  ClinicaAstra;         2023-04-11
+  FARR;                 2023-04-11
+  SantaBlanca;          2023-04-11
+  Everest;              2023-04-11
+  FALP;                 2023-04-11
+  NucleoSalud;          2023-04-11
+  Maitenes;             2023-04-11
+  Sanasalud;            2023-04-11
 `
 
 const debtors = debtorsData
@@ -44,7 +43,7 @@ const debtors = debtorsData
     const [name, date] = client.split(';')
     return {
       clientName: name.trim(),
-      dueDate: parse(date, 'yyyy-MM-dd', new Date()),
+      dueDate: parse(date.trim(), 'yyyy-MM-dd', new Date()),
     }
   })
 
