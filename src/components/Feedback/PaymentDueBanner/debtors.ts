05c4e6ee6ca1458109cc04dac9b680791c82cacd
Alejandro Lavado <alejandro.lvd@gmail.com>
2023-04-10 13:49:15 -0400

    remueve banner para varios clientes

--- src/components/Feedback/PaymentDueBanner/debtors.ts
+++ src/components/Feedback/PaymentDueBanner/debtors.ts
@@ -14,19 +14,15 @@ type Debtor = {
 const debtorsData = `
   3DentOnce16;          2023-04-11
   Falcon;               2023-04-11
-  CEOLA Temuco;         2023-04-11
   Citymed;              2023-04-11
   uandes;               2023-04-11
   CEOLA Concepcion;     2023-04-11
-  COP;                  2023-04-11
   Vitasalud;            2023-04-11
   CTMelipilla;          2023-04-11
   Cegin;                2023-04-11
   Vitales;              2023-04-11
-  UTC;                  2023-04-11
   EzioChiappe;          2023-04-11
   Integral Linares;     2023-04-11
-  2020;                 2023-04-11
   NucleoSalud;          2023-04-11
   Maitenes;             2023-04-11
   Sanasalud;            2023-04-11
