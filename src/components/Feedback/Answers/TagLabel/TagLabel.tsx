23dcf2a607b017a52c64473ec91d69190558bc0c
Alejandro Lavado <alejandro.lvd@gmail.com>
2023-05-31 10:03:56 -0400

    problema con unknown

--- src/components/Feedback/Answers/TagLabel/TagLabel.tsx
+++ src/components/Feedback/Answers/TagLabel/TagLabel.tsx
@@ -4,7 +4,10 @@ import { Tag } from '../../../../api/types/types'
 import './TagLabel.css'
 import { getPresentationForTag } from './utils'
 
-const TagLabel = ({ tag }: { tag: Tag }) => {
+const TagLabel = ({ tag }: { tag: Tag | unknown }) => {
+
+  
+
   const { colorVar, icon, label } = getPresentationForTag({ tag })
 
   return (
