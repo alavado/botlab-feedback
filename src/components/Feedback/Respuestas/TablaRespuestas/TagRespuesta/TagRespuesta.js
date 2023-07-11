c3ce93f95ff290436e9fd222e32aa3c2e8958d41
Alejandro Lavado <alejandro.lvd@gmail.com>
2023-07-11 15:12:11 -0400

    agrega indicador para llamadas en tabla de respuestas

--- src/components/Feedback/Respuestas/TablaRespuestas/TagRespuesta/TagRespuesta.js
+++ src/components/Feedback/Respuestas/TablaRespuestas/TagRespuesta/TagRespuesta.js
@@ -7,11 +7,9 @@ const maxEstrellas = 5
 const obtenerClaseTag = (tag, pregunta) => {
   if (diccionarioTags(tag)) {
     return diccionarioTags(tag).clase
-  }
-  else if (pregunta === 'Opción elegida' || pregunta === '¿Por qué no?') {
+  } else if (pregunta === 'Opción elegida' || pregunta === '¿Por qué no?') {
     return 'TagRespuesta--opcion_reagendamiento'
-  }
-  else if (!isNaN(tag)) {
+  } else if (!isNaN(tag)) {
     return 'TagRespuesta--nota'
   }
   return 'TagRespuesta TagRespuesta--vacia'
@@ -20,23 +18,41 @@ const obtenerClaseTag = (tag, pregunta) => {
 const obtenerTextoTag = (tag, pregunta) => {
   if (diccionarioTags(tag)) {
     return diccionarioTags(tag).texto
-  }
-  else if (pregunta === 'Opción elegida' || pregunta === '¿Por qué no?') {
+  } else if (pregunta === 'Opción elegida' || pregunta === '¿Por qué no?') {
     return tag
-  }
-  else if (!isNaN(tag)) {
+  } else if (!isNaN(tag)) {
     return (
       <>
-        {Array(+tag).fill(0).map((_, i) => <InlineIcon key={`estrella-activa-${i}`} className="TagRespuesta__icono_estrella" icon="mdi:star" />)}
-        {Array(Math.max(0, maxEstrellas - (+tag))).fill(0).map((_, i) => <InlineIcon key={`estrella-inactiva-${i}`} className="TagRespuesta__icono_estrella TagRespuesta__icono_estrella--inactiva" icon="mdi:star" />)}
+        {Array(+tag)
+          .fill(0)
+          .map((_, i) => (
+            <InlineIcon
+              key={`estrella-activa-${i}`}
+              className="TagRespuesta__icono_estrella"
+              icon="mdi:star"
+            />
+          ))}
+        {Array(Math.max(0, maxEstrellas - +tag))
+          .fill(0)
+          .map((_, i) => (
+            <InlineIcon
+              key={`estrella-inactiva-${i}`}
+              className="TagRespuesta__icono_estrella TagRespuesta__icono_estrella--inactiva"
+              icon="mdi:star"
+            />
+          ))}
       </>
     )
   }
   return tag
 }
 
-const TagRespuesta = ({ tag, pregunta, incluirSinRespuesta = false }) => {
-
+const TagRespuesta = ({
+  tag,
+  pregunta,
+  incluirSinRespuesta = false,
+  contactadoPorTelefono = null,
+}) => {
   if (!tag && !incluirSinRespuesta) {
     return null
   }
@@ -46,7 +62,17 @@ const TagRespuesta = ({ tag, pregunta, incluirSinRespuesta = false }) => {
 
   return (
     <div className={clase}>
-      {diccionarioTags(tag) && <InlineIcon className="TagRespuesta__icono" icon={diccionarioTags(tag).icono} />}
+      {contactadoPorTelefono && (
+        <div className="TagRespuesta__contacto_telefonico">
+          <InlineIcon icon="mdi:phone" />
+        </div>
+      )}
+      {diccionarioTags(tag) && (
+        <InlineIcon
+          className="TagRespuesta__icono"
+          icon={diccionarioTags(tag).icono}
+        />
+      )}
       <span>{texto}</span>
     </div>
   )
