1e202ce55339002edf4ac992ec3d4cc89049fb58
Alejandro Lavado <alejandro.lvd@gmail.com>
2023-08-09 13:02:47 -0400

    preserve para countup

--- src/components/Feedback/Interactions/MiniDashboard/MiniDashboard.tsx
+++ src/components/Feedback/Interactions/MiniDashboard/MiniDashboard.tsx
@@ -26,41 +26,45 @@ const MiniDashboard = () => {
         style={{ '--fill': `${stats?.answeredPercentage}%` } as CSSProperties}
       />
       <p className="MiniDashboard__title">
-        <CountUp end={stats?.answeredPercentage} />%
+        <CountUp
+          end={stats?.answeredPercentage}
+          preserveValue={true}
+          suffix="%"
+        />
       </p>
       <p className="MiniDashboard__subtitle">
-        Respondidas <CountUp end={stats?.answeredCount} /> de{' '}
-        {stats?.totalCount}
+        Respondidas <CountUp end={stats?.answeredCount} preserveValue={true} />{' '}
+        de {stats?.totalCount}
       </p>
       <div className="MiniDashboard__tags_container">
         <div className="MiniDashboard__tag_count_container">
           <TagLabel tag="SIN RESPUESTA" />
           <span className="MiniDashboard__tag_count">
-            <CountUp end={stats?.unansweredCount} />
+            <CountUp end={stats?.unansweredCount} preserveValue={true} />
           </span>
         </div>
         <div className="MiniDashboard__tag_count_container">
           <TagLabel tag="YES" />
           <span className="MiniDashboard__tag_count">
-            <CountUp end={stats?.confirmedCount} />
+            <CountUp end={stats?.confirmedCount} preserveValue={true} />
           </span>
         </div>
         <div className="MiniDashboard__tag_count_container">
           <TagLabel tag="NO" />
           <span className="MiniDashboard__tag_count">
-            <CountUp end={stats?.cancelledCount} />
+            <CountUp end={stats?.cancelledCount} preserveValue={true} />
           </span>
         </div>
         <div className="MiniDashboard__tag_count_container">
           <TagLabel tag="REAGENDA" />
           <span className="MiniDashboard__tag_count">
-            <CountUp end={stats?.rescheduledCount} />
+            <CountUp end={stats?.rescheduledCount} preserveValue={true} />
           </span>
         </div>
         <div className="MiniDashboard__tag_count_container">
           <TagLabel tag="OTRO" />
           <span className="MiniDashboard__tag_count">
-            <CountUp end={stats?.otherCount} />
+            <CountUp end={stats?.otherCount} preserveValue={true} />
           </span>
         </div>
       </div>
